d33055caaf050b97ad65aee620b1110095f2806a
Jaimish Ashar <jaimish97@gmail.com>
2020-07-12 22:19:11 +0530

    Created flutter app with basic card layout (obviously needs work).

---
+++ frontend/lib/card.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Card extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO implement card widget here and import in home.dart
+  }
+}
